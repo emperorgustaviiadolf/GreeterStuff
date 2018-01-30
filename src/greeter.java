df960ddc5d5c2c8ce359a083e3343d30f26c50d2
Unknown <19syverj@elmbrookstudents.org>
2018-01-30 10:34:35 -0600

    Created Basic Greeter

---
+++ src/greeter.java
@@ -0,0 +1,17 @@
+
+public class greeter {
+
+	private String name;
+	private int age;
+	
+	public greeter() {
+		name = "Dick";
+		age = 69;
+	}
+	
+	public greeter( String newName, int newAge) {
+		age = newAge;
+		name = newName;
+	}
+	
+}
